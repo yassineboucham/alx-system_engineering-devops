6c0e0d7d7599b7af4ba266d9e2ddede37b13f20c
yassineboucham <yassineboucham2002@gmail.com>
2024-08-16 00:18:26 +0100

    creat a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#Using Puppet, create a file in /tmp.
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
