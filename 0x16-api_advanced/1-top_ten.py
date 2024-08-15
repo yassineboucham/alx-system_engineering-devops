b71606c8c36b167bd6b2408c86e1a45515666fd0
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 15:07:17 +0100

    10_top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Top 10
+"""
+import requests
+
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
+def top_ten(subreddit):
+    base_url = "https://www.reddit.com"
+    endpoint = base_url + "/r/{}/hot.json".format(subreddit)
+    r = requests.get(endpoint, allow_redirects=False, headers=headers)
+    if r.status_code != 200:
+        print("None")
+        return
+    hot_posts = r.json().get('data').get('children')
+    for post in hot_posts[:10]:
+        print(post.get('data').get('title'))
