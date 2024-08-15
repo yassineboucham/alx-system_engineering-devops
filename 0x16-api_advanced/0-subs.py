d45636b824fef1c156bb7d0577a15f1e5f5eb31e
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 14:57:09 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Number of subs"""
     headers = {"User-Agent": "Chrome/125.0.0.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, allow_redirects=False)
+    r = requests.get(url, headers, allow_redirects=False)
     try:
         data = r.json()
         return data['data']['subscribers']
