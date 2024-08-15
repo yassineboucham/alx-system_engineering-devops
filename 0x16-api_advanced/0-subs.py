990c7996f4069923c9f9df79383b340e78ce8269
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 14:59:33 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     r = requests.get(url, headers, allow_redirects=False)
     try:
-        data = r.json()
-        return data['data']['subscribers']
+        data = r.json().get("data").get("subscribers")
+        return data
     except Exception:
         return 0
