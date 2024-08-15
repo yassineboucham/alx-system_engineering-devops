a4de7b4f905f39cf5978ea0d7e5a0ddb504f7041
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 15:13:57 +0100

    3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-"""
-count
-"""
-import requests
-
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+"""Module for task 1"""
 
 
-def page_hots(subreddit, hot_list, after_query=""):
-    """Get host posts in one page"""
-    base_url = "https://www.reddit.com"
-    endpoint = base_url + "/r/{}/hot.json".format(subreddit)
-    payload = {'after': after_query}
-    r = requests.get(
-        endpoint,
-        allow_redirects=False,
-        params=payload,
-        headers={**headers}
-    )
-    if r.status_code != 200:
-        return None
-    r = r.json()
-    after = r.get('data').get('after')
-    hot_posts = r.get('data').get('children')
-    for post in hot_posts:
-        hot_list.append(post.get('data').get('title'))
-        print('-', post.get('data').get('title'))
-    return after
+def top_ten(subreddit):
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-
-def recurse(subreddit, hot_list=[]):
-    """Get all the hot posts"""
-    if type(subreddit) is list:
-        subreddit, after = subreddit[0], subreddit[1]
-        # print("{)", subreddit, after)
-        after = page_hots(subreddit, hot_list, after)
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        after = page_hots(subreddit, hot_list)
-    if after:
-        recurse([subreddit, after], hot_list)
-    return hot_list
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
