b27d98f8bafe07d4d5fa50b2a6a419a39d305aa0
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 15:08:25 +0100

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""
+Get list of hot posts of a subreddit
+"""
+import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
+def page_hots(subreddit, hot_list, after_query=""):
+    """Get host posts in one page"""
+    base_url = "https://www.reddit.com"
+    endpoint = base_url + "/r/{}/hot.json".format(subreddit)
+    payload = {'after': after_query}
+    r = requests.get(
+        endpoint,
+        allow_redirects=False,
+        params=payload,
+        headers={**headers}
+    )
+    if r.status_code != 200:
+        return None
+    r = r.json()
+    after = r.get('data').get('after')
+    hot_posts = r.get('data').get('children')
+    for post in hot_posts:
+        hot_list.append(post.get('data').get('title'))
+    return after
+
+
+def recurse(subreddit, hot_list=[]):
+    """Get all the hot posts"""
+    if type(subreddit) is list:
+        subreddit, after = subreddit[0], subreddit[1]
+        # print("{)", subreddit, after)
+        after = page_hots(subreddit, hot_list, after)
+    else:
+        after = page_hots(subreddit, hot_list)
+    if after:
+        recurse([subreddit, after], hot_list)
+    return hot_list
