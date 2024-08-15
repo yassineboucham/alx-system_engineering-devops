f4783419586f035a8083a68f6121b355e6b062c6
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 14:53:52 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """number_of_subscribers"""
+    headers = {"User-Agent": "Chrome/125.0.0.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(url, allow_redirects=False)
     try:
