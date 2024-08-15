d5d59108aff0e8f494e34df7c1a7a3f16d7a64e8
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 14:52:22 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+    returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number_of_subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, allow_redirects=False)
+    try:
+        data = req.json()
+        return data['data']['subscribers']
+    except Exception:
+        return 0
