393874ec3fd15c2f936acf4bfb4b0c877ac75bc4
yassineboucham <yassineboucham2002@gmail.com>
2024-08-15 14:54:27 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-    returns the number of subscribers
+How many subs?
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """number_of_subscribers"""
+    """Number of subs"""
     headers = {"User-Agent": "Chrome/125.0.0.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url, allow_redirects=False)
+    r = requests.get(url, allow_redirects=False)
     try:
-        data = req.json()
+        data = r.json()
         return data['data']['subscribers']
     except Exception:
         return 0
