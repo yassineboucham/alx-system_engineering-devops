3906cc4695cb9d4549dc6f931b9dfd53e387630f
yassineboucham <yassineboucham2002@gmail.com>
2024-08-16 01:42:16 +0100

    Using Puppet, install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install flask from pip3.
 
-package { 'pythone':
+package { 'pythone3.8':
     ensure => '3.8.10',
     provider => 'pip3'
 }
@@ -13,5 +13,4 @@ package { 'flask':
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require => package['flask'],
 }
