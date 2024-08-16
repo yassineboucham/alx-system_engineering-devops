862b5e50b9f84efcab74c716fbfd214961e323af
yassineboucham <yassineboucham2002@gmail.com>
2024-08-16 02:00:54 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,22 +6,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    headers = {"User-Agent": "Mozilla/5.0"}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
+    """Number of subs"""
+    headers = {"User-Agent": "Chrome/125.0.0.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    r = requests.get(url, allow_redirects=False)
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the response is successful
-        if response.status_code == 200:
-            data = response.json()
-            # Safely access subscriber count
-            return data.get('data', {}).get('subscribers', 0)
-        
-        # If the subreddit is invalid, Reddit returns 301 or 302
-        return 0
-
-    except requests.RequestException:
-        # Catch any requests exceptions
+        data = r.json()
+        return data['data']['subscribers']
+    except Exception:
         return 0
